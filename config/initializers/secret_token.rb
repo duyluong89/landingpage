07c8e3c165a0a2e6efe9e243f6d80367ec35046a
trinh duy luong <trinh.duy.luong@kloon.vn>
2013-07-22 16:24:42 +0700

    add new site rails 3.2.13 ruby 1.9.x

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Landingpage::Application.config.secret_token = '280525f7e28b2367a3e74bca5a30f902ef207e0da6e05f80b078b132df775db4524d3ad34744df3bd4c35f4602eae37e2dc5e15b2734b9d24d02299640d693ed'
