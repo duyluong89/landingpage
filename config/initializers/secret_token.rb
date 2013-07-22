4129ef14a8875edb2acc3f9590ab65c68b3ba47b
trinh duy luong <trinh.duy.luong@kloon.vn>
2013-07-22 16:22:38 +0700

    delete  old site

--- config/initializers/secret_token.rb
+++
@@ -1,7 +0,0 @@
-# Be sure to restart your server when you modify this file.
-
-# Your secret key for verifying the integrity of signed cookies.
-# If you change this key, all old signed cookies will become invalid!
-# Make sure the secret is at least 30 characters and all random,
-# no regular words or you'll be exposed to dictionary attacks.
-Landingpage::Application.config.secret_token = 'dc7301cd7dea2162dc619c9724320959e79f9a5f9c320b18e0dffd35969b8e80af4f6e07e3418d67fc9d193379962a0125b62b5d446ff6625e7ddc1ee50417fe'
