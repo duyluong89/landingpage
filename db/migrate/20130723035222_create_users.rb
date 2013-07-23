2a5207dc310152ba5f65d4ecf72aa0ed3dc4e035
trinh duy luong <trinh.duy.luong@kloon.vn>
2013-07-23 11:13:10 +0700

    add module user, layout

---
+++ db/migrate/20130723035222_create_users.rb
@@ -0,0 +1,16 @@
+class CreateUsers < ActiveRecord::Migration
+  def change
+    create_table :users do |t|
+      t.string :username
+      t.string :password
+      t.string :userLever
+      t.string :name
+      t.string :email
+      t.string :description
+      t.boolean :editAnalytics
+      t.boolean :active
+
+      t.timestamps
+    end
+  end
+end
